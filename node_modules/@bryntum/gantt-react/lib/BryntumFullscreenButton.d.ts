a486de3aee030ee6c5adc750b73c26b0c9eabcbd
tieniber <tieniber@gmail.com>
2024-10-01 23:43:56 -0400

    remove node_modules

--- node_modules/@bryntum/gantt-react/lib/BryntumFullscreenButton.d.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ReactNode } from 'react';
-export declare class BryntumFullscreenButton extends Component {
-    private elRef;
-    private button?;
-    componentDidMount(): void;
-    componentWillUnmount(): void;
-    render(): ReactNode;
-}
